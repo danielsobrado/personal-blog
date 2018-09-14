33bca8430fdfe86b3b02f3477ec01019b426d724
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2018-09-14 15:59:02 +0400

    test

--- publish.bat
+++ publish.bat
@@ -2,5 +2,5 @@ set arg1=%1
 cd C:\Users\daniel\Documents\GitHub\personal_blog
 hugo
 robocopy /move /e C:\Users\daniel\Documents\GitHub\personal_blog\public C:\Users\daniel\Documents\GitHub\personal_blog\docs
-git commit -am %1
+git commit -am "%1"
 git push
