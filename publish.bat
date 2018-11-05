471e8f868671a9a5450c6b240d0520d5a0ad9688
Daniel <daniel@dalamarblog.com>
2018-11-05 07:41:37 +0400

    publish

--- publish.bat
+++ publish.bat
@@ -5,7 +5,8 @@ mkdir docs
 hugo
 ping 127.0.0.1 -n 2 > nul
 robocopy /move /e E:\Documents\GitHub\personal-blog\public E:\Documents\GitHub\personal-blog\docs
+cd ..
 rmdir public /s /q
 mkdir public
 git commit -am "publish"
-cd ..
+
