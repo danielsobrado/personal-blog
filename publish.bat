93b1c4b3b1aa31df961bb50be7297c201c817e4b
Daniel <daniel@dalamarblog.com>
2018-11-05 07:19:29 +0400

    publish

--- publish.bat
+++ publish.bat
@@ -1,8 +1,8 @@
 set arg1=%1
 cd C:\Users\daniel\Documents\GitHub\personal_blog
-hugo
 rmdir docs /s /q
 mkdir docs
+hugo
 robocopy /move /e C:\Users\daniel\Documents\GitHub\personal_blog\public C:\Users\daniel\Documents\GitHub\personal_blog\docs
 rmdir public /s /q
 mkdir public
